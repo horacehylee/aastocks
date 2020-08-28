030d30139f8dca5d32e88d64cc0897b3e8f56597
Horace Lee <horacehylee2@gmail.com>
2020-08-28 14:59:56 +0800

    update doc

--- doc.go
+++ doc.go
@@ -1,15 +1,18 @@
 // Package aastocks is an extractor for AAStocks financial market data.
 //
+//
 // quote, err := aastocks.Get("00006")
 // if err != nil {
 // 	logger.Fatal(err)
 // }
 // ... Use quote to get financial data (i.e. for its dividends and historical price).
 //
+//
 // Real Time Prices
 //
 // Prices can be served in real time by polling AAStocks for its price.
 // Context can be used to control and stop the real time prices.
+//
 //
 // priceChan, errChan := quote.ServePrice(context.Background(), 5*time.Second)
 // for {
@@ -20,5 +23,6 @@
 // 		logger.Printf("error: %v\n", err)
 // 	}
 // }
+//
 //
 package aastocks
